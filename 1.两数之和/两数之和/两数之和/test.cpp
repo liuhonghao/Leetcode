c689bc938c00bc6c70645ef5d86463a4ce5a3ca2
刘弘浩 <76436932+liuhonghao@users.noreply.github.com>
2021-04-20 21:47:07 +0800

    Add files via upload

---
+++ 1.两数之和/两数之和/两数之和/test.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+using namespace std;
+/*
+±©Á¦Ð´·¨ O(n^2)
+*/
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> res;
+		for (int i = 0; i<nums.size(); i++)
+		{
+			for (int j = 0; j<i; j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					res = vector<int>({ j, i });
+					break;
+				}
+			}
+			if (res.size()>0)break;
+		}
+		return res;
+	}
+};
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> res;
+		unordered_map<int, int> heap;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int another = target - nums[i];
+			if (heap.count(another))
+			{
+				res = vector<int>({ heap[another], i });
+				break;
+			}
+			heap[nums[i]] = i;
+		}
+		return res;
+	}
+}
